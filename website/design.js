1d4240b779055e98953e424ad8e4cdfebc155a31
callmeisacc@gmail.com <callmeisacc@gmail.com>
2023-04-22 07:13:17 +0000

    yh

--- website/design.js
+++ website/design.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const main = document.querySelector(".main");
 
 hamburger.addEventListener("click", () => {
    hamburger.classList.toggle("active");
    navMenu.classList.toggle("active");
+   main.classList.toggle("margin");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
    hamburger.classList.remove("active");
    navMenu.classList.remove("active");
+   main.classList.remove("margin");
 }))
